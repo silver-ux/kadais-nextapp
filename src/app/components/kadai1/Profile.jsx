c4f39c2cfd7ce61fff750952c1c6f1fe391ca28a
SHOTA <sh.studylab@gmail.com>
2024-12-19 14:18:12 +0900

    Motionでアニメーションを追加しました。

--- src/app/components/kadai1/Profile.jsx
+++ src/app/components/kadai1/Profile.jsx
@@ -1,10 +1,19 @@
+'use client';
+
 import Image from 'next/image'
 import React from 'react'
 import image from 'public/kadai1img/about.jpg'
+import { motion } from 'motion/react'
 
 export const Profile = () => {
     return (
-        <div className='sm:flex-row place-content-center container-layout mt-[70px] flex items-center flex-col'>
+        <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1.6, type: 'spring', bounce: 0.3 }}
+            viewport={{ once: true }}
+            className='sm:flex-row place-content-center container-layout mt-[70px] flex items-center flex-col'>
+
             <Image
                 src={image}
                 width={100}
@@ -12,10 +21,11 @@ export const Profile = () => {
                 style={{ borderRadius: '50%' }}
                 alt='Profile image'
             />
+
             <div className='w-full sm:w-[48%] ml-[30px]'>
                 <h3 className='font-bold'>KAKERU MIYAICHI</h3>
                 <p className='pt-2.5 text-[14.4px]'>テキストテキストテキストテキストテキストテキストテキスト<br />テキストテキストテキストテキストテキストテキストテキスト<br />テキストテキストテキストテキストテキストテキストテキスト</p>
             </div>
-        </div>
+        </motion.div>
     )
 }
--- src/app/components/kadai1/section/Bicycle.jsx
+++ src/app/components/kadai1/section/Bicycle.jsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react'
 import { Title } from '../Title'
 import Link from 'next/link'
@@ -5,6 +6,8 @@ import Image from 'next/image'
 import img1 from 'public/kadai1img/bicycle1.jpg'
 import img2 from 'public/kadai1img/bicycle2.jpg'
 import img3 from 'public/kadai1img/bicycle3.jpg'
+import { motion } from 'motion/react'
+import { spring } from 'motion';
 
 
 export const Bicycle = () => {
@@ -30,20 +33,27 @@ export const Bicycle = () => {
         <div className='pt-[100px]' id='bicycle'>
             <Title>Bicycle</Title>
             <div className='container-layout flex flex-col sm:flex-row justify-between pt-[60px] pb-24 text-center'>
-                {bicycles.map((bicycle) => (
+                {bicycles.map((bicycle, index) => (
                     <Link key={bicycle.id} href="https://www.youtube.com/" className='group block' rel="noopener noreferrer" target="_blank">
-                        <div className='sm:w-[262.8px] w-full sm:h-[174px] overflow-hidden rounded-xl'>
-                            <Image
-                                src={bicycle.image}
-                                alt='bicycle image'
-                                width={640}
-                                height={424}
-                                style={{ width: "100%", height: "100%" }}
-                                className='group-hover:scale-110 transition-all '
-                            />
-                        </div>
-                        <h3 className='font-bold my-2.5'>{bicycle.title}</h3>
-                        <p className='text-[14.4px] mb-8'>{bicycle.text}</p>
+                        <motion.div
+                            initial={{ opacity: 0, translateY: 50 }}
+                            whileInView={{ opacity: 1, translateY: 0 }}
+                            transition={{ duration: 0.7, delay: 0.3 * index }}
+                            viewport={{ once: true }}
+                        >
+                            <div className='sm:w-[262.8px] w-full sm:h-[174px] overflow-hidden rounded-xl'>
+                                <Image
+                                    src={bicycle.image}
+                                    alt='bicycle image'
+                                    width={640}
+                                    height={424}
+                                    style={{ width: "100%", height: "100%" }}
+                                    className='group-hover:scale-110 transition-all '
+                                />
+                            </div>
+                            <h3 className='font-bold my-2.5'>{bicycle.title}</h3>
+                            <p className='text-[14.4px] mb-8'>{bicycle.text}</p>
+                        </motion.div>
                     </Link>
                 ))}
             </div>
